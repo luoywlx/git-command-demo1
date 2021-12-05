a9ccb67e7d0648a5dd66fe3621426b96d4493662
luoywlx <56662444+luoywlx@users.noreply.github.com>
2021-12-05 22:48:27 +0800

    修改1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,15 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("修改1");
+        System.out.println("Hello World!");
+
     }
 }
